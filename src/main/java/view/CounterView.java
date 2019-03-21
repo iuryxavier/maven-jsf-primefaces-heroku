00958fe414de8226c938e5bdb06cec0cb8d2bf9a
Iury Xavier <iuryadones@gmail.com>
2019-03-21 17:21:11 -0300

    Add CounterView.java

---
+++ src/main/java/view/CounterView.java
@@ -0,0 +1,14 @@
+@ManagedBean
+@ViewScoped
+public class CounterView implements Serializable {
+     
+    private int number;
+ 
+    public int getNumber() {
+        return number;
+    }
+ 
+    public void increment() {
+        number++;
+    }
+}
